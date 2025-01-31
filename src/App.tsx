3085e3fb8ec9d75f10aeb91d66d06002991b93c9
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2025-01-31 19:24:46 -0300

    change font and color theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Button } from '@/components/ui/button';
 function App() {
   return (
     <>
-      <div className="flex flex-row p-40 items-center justify-center bg-violet-500 m-4">
-        <p className="font-bold text-5xl">got.it!</p>
+      <div className="flex flex-col p-40 items-center justify-center bg-main-700 m-4">
+        <p className="font-bold text-5xl py-16">Hello world!</p>
         <Button>Click me</Button>
       </div>
     </>
