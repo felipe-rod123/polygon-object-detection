ff7e69577871a41d9bdbbf864009aa36f2bb72ef
Felipe <84182706+felipe-rod123@users.noreply.github.com>
2025-02-03 16:27:23 -0300

    add eraser package

--- src/pages/draw/DrawCanvas.tsx
+++ src/pages/draw/DrawCanvas.tsx
@@ -1,17 +1,12 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
-import { DrawTool, DrawToolsEnum } from '@/types/enums/DrawToolsEnum';
-import { ToolToggle, ToolToggleEnum } from '@/types/enums/ToolToggleEnum';
+import { DrawToolsEnum } from '@/types/enums/DrawToolsEnum';
+import { ToolToggleEnum } from '@/types/enums/ToolToggleEnum';
 
-import {
-  ActiveSelection,
-  Canvas,
-  FabricObject,
-  PencilBrush,
-  Rect,
-} from 'fabric';
+import { Canvas, PencilBrush } from 'fabric';
 import { useEffect, useRef, useState } from 'react';
+
+import { EraserBrush } from '@erase2d/fabric';
+
 
 interface DrawCanvasProps {
   canvasMode: ToolToggleEnum;
@@ -29,11 +24,6 @@ const DrawCanvas: React.FC<DrawCanvasProps> = ({
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const [fabricCanvas, setFabricCanvas] = useState<Canvas | null>(null);
-
-  const mode = new ToolToggle(canvasMode);
-  const drawTool = new DrawTool(canvasDrawTool);
-
-  let _clipboard: FabricObject | ActiveSelection | null = null;
 
   useEffect(() => {
     if (!canvasRef.current || !containerRef.current) return;
@@ -54,31 +44,8 @@ const DrawCanvas: React.FC<DrawCanvasProps> = ({
     updateCanvasSize();
     window.addEventListener('resize', updateCanvasSize);
 
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (!fabricCanvas) return;
-
-      switch (event.key) {
-        case 'Delete':
-        case 'Backspace':
-          deleteSelectedObjects();
-          break;
-        case 'c':
-          if (event.ctrlKey) copyObjectsToClipboard();
-          break;
-        case 'v':
-          if (event.ctrlKey) pasteObjectsFromClipboard();
-          break;
-        case 'd':
-          if (event.ctrlKey) duplicateSelectedObjects();
-          break;
-      }
-    };
-    window.addEventListener('keydown', handleKeyDown);
-
-    // cleanup
     return () => {
       window.removeEventListener('resize', updateCanvasSize);
-      window.removeEventListener('keydown', handleKeyDown);
       canvas.dispose();
     };
   }, []);
@@ -86,22 +53,37 @@ const DrawCanvas: React.FC<DrawCanvasProps> = ({
   useEffect(() => {
     if (!fabricCanvas) return;
 
-    if (canvasMode === ToolToggleEnum.DRAW) {
-      if (drawTool.isBrush) {
-        console.log('isBrush');
-        freeDraw();
+    const updateDrawingMode = () => {
+      if (canvasMode === ToolToggleEnum.DRAW) {
+        fabricCanvas.set({ isDrawingMode: true });
+
+        switch (canvasDrawTool) {
+          case DrawToolsEnum.BRUSH:
+            brushDraw();
+            break;
+          case DrawToolsEnum.POLYGON:
+            polygonDraw();
+            break;
+          case DrawToolsEnum.ERASER:
+            eraserBrushDraw();
+            break;
+          default:
+            brushDraw();
+            break;
+        }
+      } else {
+        fabricCanvas.set({ isDrawingMode: false });
       }
-      if (drawTool.isPolygon) console.log('isPolygon');
-      if (drawTool.isEraser) console.log('isEraser');
-    } else {
-      fabricCanvas.set({ isDrawingMode: false });
-    }
+    };
+
+    updateDrawingMode();
   }, [fabricCanvas, canvasMode, canvasDrawTool, strokeColor, strokeWidth]);
 
-  const freeDraw = () => {
-    if (!fabricCanvas || !mode.isDraw) return;
-
+  const brushDraw = () => {
+    if (!fabricCanvas) return;
     fabricCanvas.set({ isDrawingMode: true });
+
+    console.log('brush');
 
     const brush = new PencilBrush(fabricCanvas);
     brush.color = strokeColor;
@@ -109,81 +91,29 @@ const DrawCanvas: React.FC<DrawCanvasProps> = ({
     fabricCanvas.set({ freeDrawingBrush: brush });
   };
 
-  const addRectangle = () => {
+  //FIXME:
+  const eraserBrushDraw = () => {
     if (!fabricCanvas) return;
 
-    const rect = new Rect({
-      left: 50,
-      top: 50,
-      fill: strokeColor,
-      width: 100,
-      height: 100,
-    });
+    console.log('eraser');
 
-    fabricCanvas.add(rect);
-    fabricCanvas.renderAll();
+    const eraser = new EraserBrush(fabricCanvas);
+    eraser.width = 30;
+
+    fabricCanvas.freeDrawingBrush = eraser;
+    fabricCanvas.isDrawingMode = true;
+  };
+
+  //FIXME:
+  const polygonDraw = () => {
+    if (!fabricCanvas) return;
+
+    console.log('polygon');
   };
 
   const clearCanvas = () => {
     if (!fabricCanvas) return;
     fabricCanvas.clear();
-  };
-
-  const copyObjectsToClipboard = () => {
-    if (!fabricCanvas) return;
-
-    const activeObject = fabricCanvas.getActiveObject();
-    if (!activeObject) return;
-
-    activeObject.clone().then(cloned => {
-      _clipboard = cloned;
-    });
-  };
-
-  const pasteObjectsFromClipboard = async () => {
-    if (!fabricCanvas || !_clipboard) return;
-
-    const clonedObj = await _clipboard.clone();
-    fabricCanvas.discardActiveObject();
-
-    clonedObj.set({
-      left: clonedObj.left + 10,
-      top: clonedObj.top + 10,
-      evented: true,
-    });
-
-    if (clonedObj instanceof ActiveSelection) {
-      clonedObj.canvas = fabricCanvas;
-      clonedObj.forEachObject((obj: FabricObject) => {
-        fabricCanvas.add(obj);
-      });
-
-      clonedObj.setCoords();
-    } else {
-      fabricCanvas.add(clonedObj);
-    }
-
-    _clipboard.top += 10;
-    _clipboard.left += 10;
-
-    fabricCanvas.setActiveObject(clonedObj);
-    fabricCanvas.requestRenderAll();
-  };
-
-  const duplicateSelectedObjects = () => {
-    if (!fabricCanvas) return;
-
-    copyObjectsToClipboard();
-    pasteObjectsFromClipboard();
-  };
-
-  const deleteSelectedObjects = () => {
-    if (!fabricCanvas) return;
-
-    const activeObject = fabricCanvas.getActiveObject();
-    if (activeObject) {
-      fabricCanvas.remove(activeObject);
-    }
   };
 
   return (
@@ -194,7 +124,6 @@ const DrawCanvas: React.FC<DrawCanvasProps> = ({
       <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full" />
       <div className="absolute top-4 left-4 flex gap-2">
         <Button onClick={clearCanvas}>Clear canvas</Button>
-        <Button onClick={addRectangle}>Add Rectangle</Button>
       </div>
     </div>
   );
